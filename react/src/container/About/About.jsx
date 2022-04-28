956c5054427c4be7125f1d98c004de177f6cc365
GuysKapen <guyskapen@gmail.com>
2022-04-28 11:39:06 -0800

    Design about section

--- react/src/container/About/About.jsx
+++ react/src/container/About/About.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import './About.scss';
+import { motion } from "framer-motion";
+import { images } from '../../constants';
 
 const About = () => {
+    const abouts = [
+        {
+            title: "Web developer",
+            description: "I am a good web developer",
+            imgUrl: images.about01
+        },
+        {
+            title: "Mobile developer",
+            description: "I am a good web developer",
+            imgUrl: images.about02
+        },
+        {
+            title: "AI engenieer",
+            description: "I am a good web developer",
+            imgUrl: images.about03
+        },
+        {
+            title: "UI/UX desginer",
+            description: "I am a good web developer",
+            imgUrl: images.about04
+        },
+    ]
     return (
-        <div>
-            
-        </div>
+        <>
+            <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+            <div className='app__profiles'>
+                {abouts.map((about, index) => (
+                    <motion.div
+                        whileInView={{ opacity: [0, 1], x: [120, 0] }}
+                        whileHover={{ scale: 1.12 }}
+                        transition={{ duration: 0.5, ease: "easeInOut" }}
+                        className='app__profile-item cursor-pointer'
+                        key={`about-${index}`}>
+                        <img src={about.imgUrl} alt={about.title} />
+                        <h2 className='bold-text mt-4'>{about.title}</h2>
+                        <p className='p-text mt-2'>{about.description}</p>
+                    </motion.div>
+                ))}
+            </div>
+
+        </>
     );
 }
 
