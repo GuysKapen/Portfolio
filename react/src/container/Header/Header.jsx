1b88c6e9e968bfd7df92a796f8053b90a375ed39
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-02-28 07:18:55 +0700

    Update header

--- react/src/container/Header/Header.jsx
+++ react/src/container/Header/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import "./Header.scss";
 import { images } from "../../constants"
 import { Section } from "../../layout"
+import PlayButton from '../PlayButton';
 
 const Header = () => {
     const scaleVariants = {
@@ -42,10 +43,17 @@ const Header = () => {
             <motion.div whileInView={{ opacity: [0, 1] }}
                 transition={{ duration: 0.5, delayChildren: 0.5 }}
                 className='app__header-img'>
-                <img src={images.profile} alt="image_profile" />
+                <div className='w-full invisible lg:visible absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 px-8 z-10'>
+                    <h1 className='text-6xl text-center font-black'>Guys Robot</h1>
+                    <p className='mt-8 w-1/2 text-2xl text-center mx-auto'>Web app developer and AI engineer</p>
+                    <div className='mx-auto mt-8 w-48'>
+                        <PlayButton />
+                    </div>
+                </div>
+                {/* <img src={images.profile} alt="image_profile" /> */}
                 <motion.img whileInView={{ scale: [0, 1] }}
                     transition={{ duration: 0.5, ease: "easeInOut" }}
-                    className='overlay_circle'
+                    className='overlay_circle top-0'
                     src={images.circle}
                     alt="profile_circle" />
 
